8cd358dd06ab755b54ce1248cb0c55e8f3592f41
Iri <89229099+IAgretti@users.noreply.github.com>
2023-10-26 09:05:17 -0300

    Ejercicio clase Operación

---
+++ Clase23/Clase23/Operacion.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase23
+{
+    public class Operacion
+    {
+        protected int valor1, valor2, resultado;
+
+        public int Valor1
+        {
+            get 
+            { 
+                return valor1;
+            }
+            set 
+            {
+                valor1 = value;
+            }
+        }
+        public int Valor2
+        {
+            get { return valor2; }
+            set { valor2 = value; }
+        }
+        public int Resultado { get; set; }
+
+    }
+    public class Suma: Operacion
+    {
+        public void Operar()
+        {
+            Resultado = Valor1 + Valor2;
+        }
+       
+    }
+    public class Resta: Operacion
+    {
+        public void Operar()
+        {
+            Resultado = Valor1 - Valor2;
+        }
+    }
+}
---
+++ Clase23/Clase23/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Clase23
+{
+   
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Suma s = new Suma();
+            s.Valor1 = 10;
+            s.Valor2 = 7;
+            s.Operar();
+            Console.WriteLine("La suma de " + s.Valor1 + " y " + s.Valor2 + " es " + s.Resultado + ".");
+            Resta r = new Resta();
+            r.Valor1 = 8;
+            r.Valor2 = 4;
+            r.Operar();
+            Console.WriteLine("La resta de " + r.Valor1 + " y " + r.Valor2 + " es " + r.Resultado + ".");
+            Console.ReadKey();
+        }
+    }
+}
